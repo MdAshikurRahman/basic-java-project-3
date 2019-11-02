7a89e8f68632a606e413c99eacb5d3958c973b5f
MdAshikurRahman <mdashikurrahman.online@gmail.com>
2019-11-02 22:15:00 +0600

    new javaproject3.java file create

---
+++ JaveProject3.java
@@ -0,0 +1,30 @@
+
+package javeproject3;
+
+
+public class JaveProject3 {
+
+  
+    public static void main(String[] args) {
+    
+       boolean b;
+       boolean g = true;
+       char c;
+       short s = 324;
+       int i = 478456865;
+       float f = 10.3f;
+       double d = 10.5;
+       
+       b = true;
+       System.out.println("b="+b);
+       c = 'a';
+       System.out.println("c = "+c);
+       System.out.println("g = "+g);
+       System.out.println("s = "+s);
+       System.out.println("i = "+i);
+       System.out.println("f = "+f);
+       System.out.println("d = "+d);
+              
+    }
+    
+}
